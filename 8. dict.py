4b0c44279f9a0171a75a34aa67accfdcfe9fceb0
42TLwind <sylhypocrite@gmail.com>
2025-10-13 17:41:21 +0000

    Oct 14, 2025, 1:41 AM

--- 8. dict.py
+++ 8. dict.py
@@ -1,4 +1,9 @@
+# in python, the dict is a series of key-value pairs
 # a simple dictionary example
 alien_0 = {"color": "green", "points": 5}
 print(alien_0["color"])
 print(alien_0["points"])
+
+# now you can get the value associated with a key
+new_points = alien_0["points"]
+print(f"You just earned {new_points} points!")
