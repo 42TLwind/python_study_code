ae660ca5e44f3279ad7f61cb99ae345ba518f568
42TLwind <sylhypocrite@gmail.com>
2025-10-13 17:42:18 +0000

    Oct 14, 2025, 1:42 AM

--- 8. dict.py
+++ 8. dict.py
@@ -7,3 +7,8 @@ print(alien_0["points"])
 # now you can get the value associated with a key
 new_points = alien_0["points"]
 print(f"You just earned {new_points} points!")
+
+# you can add new key-value pairs to a dictionary
+alien_0["x_position"] = 0
+alien_0["y_position"] = 25
+print(alien_0)
